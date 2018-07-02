4dd8299bbb076f746b01e257b1e6df0bd128f82d
Nate Miller <nate@natemiller.org>
2018-07-02 12:37:58 -0500

    Opens tray with different number of elements

--- js/tray.js
+++ js/tray.js
@@ -6,7 +6,40 @@ class Tray {
 	}
 
 	open(trayContentId) {
-		console.log("Open", trayContentId);
+		this.load(trayContentId);
+	}
+
+	load(trayContentId) {
+		this.empty();
+
+		let tpl = document.getElementById(trayContentId);
+		let clone = document.importNode(tpl.content, true);
+		this.element.appendChild(clone);
+
+		// assumes a OL - LI structure. Find the computed width of the tray
+		let trayWidth = 0;
+		let container = this.element.children[0]; // the OL
+
+		for (var i=0; i < container.children.length; i++) {
+			trayWidth += container.children[i].clientWidth;
+		}
+
+		if (trayWidth < this.element.clientWidth) {
+			// remove fixed width if applied
+			container.style.width = "";
+		} else {
+			// set the OL to the width of its children
+			container.style.width = trayWidth + "px";
+		}
+
+		
+	}
+
+	// removes all items in the tray
+	empty() {
+		while (this.element.firstChild) {
+			this.element.removeChild(this.element.firstChild);
+		}
 	}
 }
 
